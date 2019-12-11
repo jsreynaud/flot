0852e2b0599707b883eca5cc3cc9daf153f9a479
Jean-Samuel Reynaud <js@qowisio.com>
2019-12-11 14:51:45 +0100

    sens

--- jquery.flot.threshold.js
+++ jquery.flot.threshold.js
@@ -44,11 +44,11 @@ You may need to check for this in hover events.
 
 (function ($) {
     var options = {
-        series: { threshold: null } // or { below: number, color: color spec}
+        series: { threshold: null } // or { value: number, color: color spec}
     };
     
     function init(plot) {
-        function thresholdData(plot, s, datapoints, below, color) {
+        function thresholdData(plot, s, datapoints, value, color, sens) {
             var ps = datapoints.pointsize, i, x, y, p, prevp,
                 thresholded = $.extend({}, s); // note: shallow copy
 
@@ -71,16 +71,18 @@ You may need to check for this in hover events.
                 y = origpoints[i + 1];
 
                 prevp = p;
-                if (y < below)
+                if (sens==1 && y > value)
+                    p = threspoints;
+                else if (sens==-1 && y < value)
                     p = threspoints;
                 else
                     p = newpoints;
 
                 if (addCrossingPoints && prevp != p && x != null
                     && i > 0 && origpoints[i - ps] != null) {
-                    var interx = x + (below - y) * (x - origpoints[i - ps]) / (y - origpoints[i - ps + 1]);
+                    var interx = x + (value - y) * (x - origpoints[i - ps]) / (y - origpoints[i - ps + 1]);
                     prevp.push(interx);
-                    prevp.push(below);
+                    prevp.push(value);
                     for (m = 2; m < ps; ++m)
                         prevp.push(origpoints[i + m]);
                     
@@ -89,7 +91,7 @@ You may need to check for this in hover events.
                     for (m = 2; m < ps; ++m)
                         p.push(origpoints[i + m]);
                     p.push(interx);
-                    p.push(below);
+                    p.push(value);
                     for (m = 2; m < ps; ++m)
                         p.push(origpoints[i + m]);
                 }
@@ -118,15 +120,14 @@ You may need to check for this in hover events.
             
             if (s.threshold instanceof Array) {
                 s.threshold.sort(function(a, b) {
-                    return a.below - b.below;
+                    return a.value - b.value;
                 });
-                
                 $(s.threshold).each(function(i, th) {
-                    thresholdData(plot, s, datapoints, th.below, th.color);
+                    thresholdData(plot, s, datapoints, th.value, th.color, th.sens);
                 });
             }
             else {
-                thresholdData(plot, s, datapoints, s.threshold.below, s.threshold.color);
+                thresholdData(plot, s, datapoints, s.threshold.value, s.threshold.color, s.threshold.sens);
             }
         }
         
